0fef2daacb5253ba54217ffe53bc0e56f2e9c527
Eda Kırcı <edakirci@gmail.com>
2023-12-18 18:19:12 +0300

    start of shuffle

--- src/Cards.java
+++ src/Cards.java
@@ -1,31 +1,71 @@
+import java.util.Random;
+import java.util.Arrays;
 public class Cards {
-    private int[] cardNums = {1,2,3,4,5,6, 7,8,9,10};
+    Random rd = new Random();
+
+    private int[] cardNums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     private String[] colors = {"blue", "green", "red", "yellow"};
     private String[] gameDeck = new String[colors.length * cardNums.length];
     private int num = 0;
 
-    public Cards(int[]cd, String[] c,String[] gd,int n) {
+    public Cards(int[] cd, String[] c, String[] gd, int n) {
         cd = cardNums;
         c = colors;
         gd = gameDeck;
         n = num;
     }
-    public void setCARDNUMS (int[] cd) {cd=cardNums;}
-    public void setCOLORS (String[] c) {c=colors;}
-    public void setCAMEDECK (String[] gd) {gd=gameDeck;}
-    public void setNUM(int n) {n=num;}
-    public int[] getCARDNUMS() {return cardNums;}
-    public String[] getCOLORS() {return colors;}
-    public String[] getGAMEDECK() {return gameDeck;}
-    public int getNUM() {return num;}
+
+    public void setCARDNUMS(int[] cd) {
+        cd = cardNums;
+    }
+
+    public void setCOLORS(String[] c) {
+        c = colors;
+    }
+
+    public void setCAMEDECK(String[] gd) {
+        gd = gameDeck;
+    }
+
+    public void setNUM(int n) {
+        n = num;
+    }
+
+    public int[] getCARDNUMS() {
+        return cardNums;
+    }
+
+    public String[] getCOLORS() {
+        return colors;
+    }
+
+    public String[] getGAMEDECK() {
+        return gameDeck;
+    }
+
+    public int getNUM() {
+        return num;
+    }
 
     public String[] Deck() {
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < cardNums.length ;j++){
+            for (int j = 0; j < cardNums.length; j++) {
                 gameDeck[num] = colors[i] + " " + cardNums[j];
                 num++;
             }
         }
         return gameDeck;
+    }
+
+    public String[] Shuffle(String[] arr, int a) {
+        arr = gameDeck;
+        a = arr.length;
+        for (int i=a-1;i>0;i--) {
+            int j = rd.nextInt(i + 1);
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        return Arrays.toString(arr);
     }
 }
